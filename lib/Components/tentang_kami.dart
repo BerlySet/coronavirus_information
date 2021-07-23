4b8025e8e01a40f20d2ffefbd4876143fb490c3b
Berly Setiawan <berly_set22@apps.ipb.ac.id>
2021-07-23 13:10:37 +0700

    webview aman

--- lib/Components/tentang_kami.dart
+++ lib/Components/tentang_kami.dart
@@ -10,6 +10,24 @@ class TentangKami extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      if (constraints.maxWidth > 800) {
+        return WebTentangKami();
+      } else {
+        return MobileTentangKami();
+      }
+    });
+  }
+}
+
+class MobileTentangKami extends StatelessWidget {
+  const MobileTentangKami({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
         Align(
@@ -131,6 +149,145 @@ class TentangKami extends StatelessWidget {
             ),
           ),
         ),
+      ],
+    );
+  }
+}
+
+class WebTentangKami extends StatelessWidget {
+  const WebTentangKami({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            "Tentang Kami",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ComingSoon();
+                }));
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.48,
+                height: 55,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(4)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset('images/sumber_info_icon.png'),
+                    Container(
+                        width: 225,
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Sumber Informasi',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              'Kami menampilkan data dari sumber terpercaya',
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 10,
+                              ),
+                            )
+                          ],
+                        )),
+                    Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.rotationY(math.pi),
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.black,
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ComingSoon();
+                }));
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.48,
+                height: 55,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset('images/survei_icon.png'),
+                    Container(
+                        width: 225,
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Survei Kepuasan Pengguna',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              'Berikan kami komentar anda mengenai aplikasi',
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 10,
+                              ),
+                            )
+                          ],
+                        )),
+                    Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.rotationY(math.pi),
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.black,
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
--- lib/Components/update_terkini.dart
+++ lib/Components/update_terkini.dart
@@ -2,12 +2,9 @@ import 'package:coronavirus_information/Constants/Warna.dart';
 import 'package:flutter/material.dart';
 
 class UpdateTerkini extends StatelessWidget {
-
   final String judul;
 
-  const UpdateTerkini({
-    Key? key, required this.judul
-  }) : super(key: key);
+  const UpdateTerkini({Key? key, required this.judul}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,91 +23,241 @@ class UpdateTerkini extends StatelessWidget {
             )
           ],
         ),
-        SizedBox(height: 10,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-              decoration: BoxDecoration(
-                border: Border.all(), borderRadius: BorderRadius.circular(4)
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('images/positif_icon.png'),
-                  Divider(),
-                  Text(
-                    '6760',
-                    style: TextStyle(
-                        color: ungu,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                  Text(
-                    'Positif',
-                    style: TextStyle(
-                        color: textPrimColor,
-                        fontSize: 10),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-              decoration: BoxDecoration(
-                border: Border.all(), borderRadius: BorderRadius.circular(4)
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('images/sembuh_icon.png'),
-                  Divider(),
-                  Text(
-                    '6760',
-                    style: TextStyle(
-                        color: hijau,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                  Text(
-                    'Sembuh',
-                    style: TextStyle(
-                        color: textPrimColor,
-                        fontSize: 10),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-              decoration: BoxDecoration(
-                border: Border.all(), borderRadius: BorderRadius.circular(4)
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('images/meninggal_icon.png'),
-                  Divider(),
-                  Text(
-                    '6760',
-                    style: TextStyle(
-                        color: merah,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                  Text(
-                    'Meninggal',
-                    style: TextStyle(
-                        color: textPrimColor,
-                        fontSize: 10),
-                  ),
-                ],
-              ),
-            ),
-          ],
+        SizedBox(
+          height: 10,
         ),
+        LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > 800) {
+            return Web();
+          } else {
+            return Mobile();
+          }
+        }),
       ],
+    );
+  }
+}
+
+class Web extends StatelessWidget {
+  const Web({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        PositifWeb(),
+        SembuhWeb(),
+        MeninggalWeb(),
+      ],
+    );
+  }
+}
+
+
+class Mobile extends StatelessWidget {
+  const Mobile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        PositifBox(),
+        SembuhBox(),
+        MeninggalBox(),
+      ],
+    );
+  }
+}
+
+class MeninggalBox extends StatelessWidget {
+  const MeninggalBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(4)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/meninggal_icon.png'),
+          Divider(),
+          Text(
+            '6760',
+            style: TextStyle(
+                color: merah, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          Text(
+            'Meninggal',
+            style: TextStyle(color: textPrimColor, fontSize: 10),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SembuhBox extends StatelessWidget {
+  const SembuhBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(4)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/sembuh_icon.png'),
+          Divider(),
+          Text(
+            '6760',
+            style: TextStyle(
+                color: hijau, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          Text(
+            'Sembuh',
+            style: TextStyle(color: textPrimColor, fontSize: 10),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PositifBox extends StatelessWidget {
+  const PositifBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(4)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/positif_icon.png'),
+          Divider(),
+          Text(
+            '6760',
+            style: TextStyle(
+                color: ungu, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          Text(
+            'Positif',
+            style: TextStyle(color: textPrimColor, fontSize: 10),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PositifWeb extends StatelessWidget {
+  const PositifWeb({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      width: MediaQuery.of(context).size.width * 0.25,
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(4)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/positif_icon.png'),
+          Text(
+            '192,575,255',
+            style: TextStyle(
+                color: ungu, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          Text(
+            'Positif',
+            style: TextStyle(color: textPrimColor, fontSize: 10),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MeninggalWeb extends StatelessWidget {
+  const MeninggalWeb({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      width: MediaQuery.of(context).size.width * 0.25,
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(4)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/meninggal_icon.png'),
+          Text(
+            '4,135,810',
+            style: TextStyle(
+                color: merah, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          Text(
+            'Meninggal',
+            style: TextStyle(color: textPrimColor, fontSize: 10),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SembuhWeb extends StatelessWidget {
+  const SembuhWeb({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      width: MediaQuery.of(context).size.width * 0.25,
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(4)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/sembuh_icon.png'),
+          Text(
+            '126,668,210',
+            style: TextStyle(
+                color: hijau, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          Text(
+            'Sembuh',
+            style: TextStyle(color: textPrimColor, fontSize: 10),
+          ),
+        ],
+      ),
     );
   }
 }
